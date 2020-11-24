e5f05ea7686ef1eeca68d8853a79f01fd938f3c7
noel balthazart <n031.b@hotmail.com>
2020-11-24 12:28:17 +0000

    actions posts added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+    @game = Game.find(params[:game_id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @game = Game.find(params[:game_id])
+    @post.game = @game
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path, notice: "Post was succesfully created."
+    else
+      render :new
+    end
   end
 
   def discussion
+    @game = Game.find(params[:game_id])
+    @discussion = @game.posts.where(category: "discussion")
   end
 
   def devlogs
+    @game = Game.find(params[:game_id])
+    @devlog = @game.posts.where(category: "devlog")
   end
 
   private
