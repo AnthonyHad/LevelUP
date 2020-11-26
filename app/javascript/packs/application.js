f694f46b70a917e79a238e21f70f0ca38ee941ea
Zuzanna Mila <zuza.mila@onet.pl>
2020-11-26 16:07:13 +0100

    pop up window and carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import "@mdi/font/css/materialdesignicons.css";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
