f1d061bd13e4406604f2fdfd8d54cefcb304613b
Zuzanna Mila <zuza.mila@onet.pl>
2020-11-30 13:17:03 +0100

    no re-loading

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,8 +3,9 @@ class RepliesController < ApplicationController
     @reply = Reply.new(reply_params)
     @reply.user = current_user
     @reply.post = Post.find(params[:post_id])
+
     if @reply.save
-      redirect_to request.referer #referes to the last "get" page
+      redirect_to game_path(@reply.post.game, anchor: "reply-#{@reply.id}")
     else
       flash[:error] = "Something went wrong try again please!" #flashed to the user an error message if it doesn't save
       redirect_to request.referer
