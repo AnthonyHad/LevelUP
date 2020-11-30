7f2e795bdbd40169e243eb40651366129779b8ee
Anthony Hadwan <anthony.hadwan@hec.edu>
2020-11-30 16:50:47 +0000

    Added filters to the games page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,10 +2,32 @@ class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @games = Game.all
-    if params[:query].present?
-      @games = Game.search_by_title_and_description_and_category(params[:query])
+    if params[:search].present?
+      search = params[:search]
+      if search[:platform].last.present? && search[:price].present?
+        @games = Game.where(platform: search[:platform]).where("price_cents < ?", search[:price].to_i)
+      elsif search[:platform].last.empty?
+        @games = Game.where("price_cents < ?", search[:price].to_i)
+      elsif search[:platform].last.present?
+        @games = Game.where(platform: search[:platform])
+      else
+        @games = Game.all
+      end
+    else
+      @games = Game.all
     end
+
+      # elsif search[:platform].length > 2 && !search[:price].last.empty?
+      #   @games = (Game.where(platform: search[:platform][1]).and(Game.where(platform: search[:platform][2]))).where("price_cents < ?", search[:price].last.to_i)
+    # if params[:query].present?
+    #   @games = Game.search_by_title_and_description_and_category(params[:query])
+    # elsif params[:platform].present?
+    #   @games = @games.where(platform: params[:platform])
+    # elsif params[:price_cents].present?
+    #   if params[:price_cents] == "£10 or less"
+    #     @games = @games.where("price_cents < ?",1000)
+    #   end
+    # end
   end
 
   def show
