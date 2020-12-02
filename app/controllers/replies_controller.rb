68b6d6a025aedc7daf95f94ad5a6da3874e9e152
noel balthazart <n031.b@hotmail.com>
2020-12-02 17:35:08 +0000

    reply direction fixed

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,9 +3,13 @@ class RepliesController < ApplicationController
     @reply = Reply.new(reply_params)
     @reply.user = current_user
     @reply.post = Post.find(params[:post_id])
-
+    
     if @reply.save
-      redirect_to game_path(@reply.post.game, anchor: "reply-#{@reply.id}")
+      if @reply.post.category == "discussion"
+        redirect_to discussion_game_posts_path(@reply.post.game, anchor: "reply-#{@reply.id}")
+      else
+        redirect_to game_path(@reply.post.game, anchor: "reply-#{@reply.id}")
+      end
     else
       flash[:error] = "Something went wrong try again please!" #flashed to the user an error message if it doesn't save
       redirect_to request.referer
