481734e02a17c2ef9724abe282a2dc414ddb367b
Alexander Haumer <alexander.haumer@me.com>
2020-12-03 12:12:52 +0000

    added mentions in Post replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,7 +3,11 @@ class RepliesController < ApplicationController
     @reply = Reply.new(reply_params)
     @reply.user = current_user
     @reply.post = Post.find(params[:post_id])
-    
+    match_data = @reply.content.match(/@(\w+)/)
+    if match_data
+      @reply.content = @reply.content.gsub(match_data[0], "<a href='/games/26'>#{match_data[0]}</a> ")
+    end
+
     if @reply.save
       if @reply.post.category == "discussion"
         redirect_to discussion_game_posts_path(@reply.post.game, anchor: "reply-#{@reply.id}")
